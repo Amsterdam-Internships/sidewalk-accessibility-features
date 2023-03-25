92ddfa9b6d188059e8949a5cc1e0de242c963b5a
imandrealombardo <andrealombardo2m@gmail.com>
2023-03-25 23:02:37 +0100

    Upload requirements.txt

--- backproject.py
+++ backproject.py
@@ -1,6 +1,8 @@
 '''Backproject the bounding boxes found in the left and right images to the equirectangular
 panoramic image, preparing them as input for the triangulation algorithm.'''
 
+from tqdm import tqdm
+import vrProjector as vrProjector
 import argparse
 import os
 import re
@@ -17,20 +19,17 @@ import matplotlib.pyplot as plt
 
 import numpy as np
 sys.path.append("lib")
-import vrProjector as vrProjector
 
-from tqdm import tqdm
-    
 
 def resize_masks(args, directory):
     '''The function resizes the output masks from the model
     to 512x512. We assume we use MOVE which outputs smaller masks.'''
     path = os.path.join(os.path.dirname(args.input_dir), 'masks_resized')
-    directory  = os.path.join(os.path.dirname(args.input_dir), 'masks')
+    directory = os.path.join(os.path.dirname(args.input_dir), 'masks')
 
     # Make a new directory to save the resized masks in
     if not os.path.exists(path):
-            os.makedirs(path)
+        os.makedirs(path)
 
     # Import all the masks in res/dataset/centrum_west_small/masks and resize them to 512x512
     print(f'Resizing {len(os.listdir(directory))} masks...')
@@ -58,9 +57,11 @@ def resize_masks(args, directory):
                     mask.save(os.path.join(new_mask_path, 'right.png'))
                 else:
                     continue
-            
+
 
 '''https://stackoverflow.com/questions/49494337/encode-numpy-array-using-uncompressed-rle-for-coco-dataset'''
+
+
 def binary_mask_to_rle(binary_mask):
     rle = {'counts': [], 'size': list(binary_mask.shape)}
     counts = rle.get('counts')
@@ -81,6 +82,7 @@ def binary_mask_to_rle(binary_mask):
 
     return rle
 
+
 def find_masks(pano_path, pano):
     # The idea is to use the image equirectangular.png in res/ and the colors HSV
     # (yellow: 57.9, 100, 100) and (blue: 240, 100, 100)
@@ -94,24 +96,29 @@ def find_masks(pano_path, pano):
     # Define the RGB values for blue
     blue_rgb = np.array([0, 0, 255])
     # Convert it to HSV
-    yellow_hsv = cv2.cvtColor(np.uint8([[yellow_rgb]]), cv2.COLOR_RGB2HSV)[0][0]
+    yellow_hsv = cv2.cvtColor(
+        np.uint8([[yellow_rgb]]), cv2.COLOR_RGB2HSV)[0][0]
     blue_hsv = cv2.cvtColor(np.uint8([[blue_rgb]]), cv2.COLOR_RGB2HSV)[0][0]
 
     # Threshold the image based on the HSV values
     yellow_lower = np.array([yellow_hsv[0] - 10, yellow_hsv[1], yellow_hsv[2]])
     yellow_upper = np.array([yellow_hsv[0] + 10, 255, 255])
-    yellow_mask = cv2.inRange(cv2.cvtColor(pano_mask, cv2.COLOR_RGB2HSV), yellow_lower, yellow_upper)
+    yellow_mask = cv2.inRange(cv2.cvtColor(
+        pano_mask, cv2.COLOR_RGB2HSV), yellow_lower, yellow_upper)
 
     blue_lower = np.array([blue_hsv[0] - 10, blue_hsv[1], blue_hsv[2]])
     blue_upper = np.array([blue_hsv[0] + 10, 255, 255])
-    blue_mask = cv2.inRange(cv2.cvtColor(pano_mask, cv2.COLOR_RGB2HSV), blue_lower, blue_upper)
+    blue_mask = cv2.inRange(cv2.cvtColor(
+        pano_mask, cv2.COLOR_RGB2HSV), blue_lower, blue_upper)
 
     # Fill any small holes inside the masks
-    yellow_mask = cv2.morphologyEx(yellow_mask, cv2.MORPH_CLOSE, np.ones((25, 25), np.uint8))
-    blue_mask = cv2.morphologyEx(blue_mask, cv2.MORPH_CLOSE, np.ones((25, 25), np.uint8))
+    yellow_mask = cv2.morphologyEx(
+        yellow_mask, cv2.MORPH_CLOSE, np.ones((25, 25), np.uint8))
+    blue_mask = cv2.morphologyEx(
+        blue_mask, cv2.MORPH_CLOSE, np.ones((25, 25), np.uint8))
 
     # Load the image
-    img = cv2.imread(os.path.join(pano_path,pano) + '.png')
+    img = cv2.imread(os.path.join(pano_path, pano) + '.png')
 
     # Convert all the image (img) but the pixels corresponding to the yellow_mask locations to black
     left_image_mask = img.copy()
@@ -122,6 +129,7 @@ def find_masks(pano_path, pano):
     right_image_mask[blue_mask != 255] = 0
 
     return [left_image_mask, right_image_mask]
+
 
 def backproject_masks(args, directory):
     # The idea is to create a black left image and just draw the bounding box on it. Then use
@@ -142,7 +150,8 @@ def backproject_masks(args, directory):
     print('Number of panos before filtering:', len(panos))
 
     # Skip the ones we already processed
-    panos = [pano for pano in panos if pano not in [pano['pano_id'] for pano in input_coco_format]]
+    panos = [pano for pano in panos if pano not in [pano['pano_id']
+                                                    for pano in input_coco_format]]
     # Print number of panos after filtering
     print('Number of panos after filtering:', len(panos))
 
@@ -153,18 +162,17 @@ def backproject_masks(args, directory):
     for pano in tqdm(panos):
         print('Processing panorama', pano)
         pano_path = os.path.join(args.input_dir, pano)
-    
-        #source = vrProjector.EquirectangularProjection()
-        #source.loadImage(pano_path)
-        #cb = vrProjector.CubemapProjection()
-        #cb.initImages(512,512)
-        #cb.reprojectToThis(source)
+
+        # source = vrProjector.EquirectangularProjection()
+        # source.loadImage(pano_path)
+        # cb = vrProjector.CubemapProjection()
+        # cb.initImages(512,512)
+        # cb.reprojectToThis(source)
 
         # Make a directory for the backprojected panorama
         pano_path = os.path.join(directory, pano)
         if not os.path.exists(pano_path):
             os.makedirs(pano_path)
-
 
         # Make bottom,top,front,back 512x512 black images
         bottom = Image.new('RGB', (512, 512), (0, 0, 0))
@@ -182,14 +190,14 @@ def backproject_masks(args, directory):
         front.save(front_path)
         back.save(back_path)
 
-        #reprojected_pano_path = os.path.join(os.path.dirname(args.input_dir), 'reprojected', pano)
+        # reprojected_pano_path = os.path.join(os.path.dirname(args.input_dir), 'reprojected', pano)
 
-        #front_path = os.path.join(reprojected_pano_path, 'front.png')
-        #back_path = os.path.join(reprojected_pano_path, 'back.png')
+        # front_path = os.path.join(reprojected_pano_path, 'front.png')
+        # back_path = os.path.join(reprojected_pano_path, 'back.png')
 
         masks_path = os.path.join(panos_path, pano)
 
-        # Load the left and right masks path. 
+        # Load the left and right masks path.
         # If they don't exist, make a 512x512 black image and save it in the folder
         left_path = os.path.join(masks_path, 'left.png')
         right_path = os.path.join(masks_path, 'right.png')
@@ -199,11 +207,12 @@ def backproject_masks(args, directory):
         if not os.path.exists(right_path):
             right = Image.new('RGB', (512, 512), (0, 0, 0))
             right.save(right_path)
-    
+
         source = vrProjector.CubemapProjection()
-        source.loadImages(front_path, right_path, back_path, left_path, top_path, bottom_path)
+        source.loadImages(front_path, right_path, back_path,
+                          left_path, top_path, bottom_path)
         eq = vrProjector.EquirectangularProjection()
-        eq.initImage(2000,1000)
+        eq.initImage(2000, 1000)
         eq.reprojectToThis(source)
         eq.saveImage(os.path.join(pano_path, f'{pano}.png'))
 
@@ -216,7 +225,7 @@ def backproject_masks(args, directory):
         # Convert the masks to panorama sizes ones
         pano_masks = find_masks(pano_path, pano)
 
-        #original_image_path = os.path.join(os.path.dirname(args.input_dir), 'reoriented', pano)
+        # original_image_path = os.path.join(os.path.dirname(args.input_dir), 'reoriented', pano)
 
         # Prepare the data for the triangulation: make a .json file as a list of dictionaries,
         # where each dictionary contains the pano_id, the bounding box and the mask
@@ -227,18 +236,19 @@ def backproject_masks(args, directory):
             pano_mask = np.where(pano_mask > 0, 1, 0)
 
             # Convert the mask to RLE
-            rles = mask_util.encode(np.array(pano_mask, order="F", dtype="uint8"))
+            rles = mask_util.encode(
+                np.array(pano_mask, order="F", dtype="uint8"))
 
             # Decode rle['counts'] to be able to save them in the json file
             for rle in rles:
                 rle['counts'] = rle['counts'].decode('utf-8')
-
 
             input_coco_format.append({'pano_id': pano, 'segmentation': rle})
 
     # Save input_coco_format
     with open(os.path.join(directory, 'input_coco_format.json'), 'w') as f:
         json.dump(input_coco_format, f)
+
 
 def main(args):
     # Replace everything that is not a character with an underscore in neighbourhood string, and make it lowercase
@@ -250,21 +260,21 @@ def main(args):
 
     # Create the output directory if it doesn't exist
     directory = os.path.join(os.path.dirname(args.input_dir), 'backprojected')
-    
+
     if not os.path.exists(directory):
         os.makedirs(directory)
 
     resize_masks(args, directory)
     backproject_masks(args, directory)
 
+
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
-   
-    parser.add_argument('--input_dir', type=str, default = 'res/dataset')
+    parser.add_argument('--input_dir', type=str, default='res/dataset')
     parser.add_argument('--neighbourhood', type=str, default='osdorp')
     parser.add_argument('--quality', type=str, default='full')
-    
+
     args = parser.parse_args()
-    
+
     main(args)

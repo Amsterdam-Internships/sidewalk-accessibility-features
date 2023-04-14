5886b9de457e5234e091ec7ed27454ebc4086a18
imandrealombardo <60581994+imandrealombardo@users.noreply.github.com>
2023-04-14 20:25:40 +0200

    generalize preprocess_panos for cluster usage

--- preprocess_panos.py
+++ preprocess_panos.py
@@ -1,5 +1,5 @@
-'''Script that extracts the panoramas from ProjectSidewalk download folders, put them in a single folder,
-creates a list of their IDs, scrape the panorama headings and lat/long coordinates from ProjectSidewalk API.
+'''Script that scrapes the panorama headings and lat/long coordinates of the panoramas downloaded
+from ProjectSidewalk API using DownloadRunner.py in lib/sidewalk-panorama-tools.
 API endpoint: https://sidewalk-amsterdam.cs.washington.edu/adminapi/panos
 
 @author: Andrea Lombardo
@@ -7,43 +7,12 @@ API endpoint: https://sidewalk-amsterdam.cs.washington.edu/adminapi/panos
 
 import argparse
 import os
-import re
-import shutil
 import http.client
 import json
-import pandas as pd
-
-import psutil
-import concurrent.futures
 from tqdm import tqdm
 
-def move_panos(source_folder, destination_folder):
-    # Make a counter for each image the script finds
-    counter = 0
-
-    # Loop through each folder in the source folder
-    for foldername in tqdm(os.listdir(source_folder)):
-        folderpath = os.path.join(source_folder, foldername)
-
-        # Skip any non-folder items in the source folder
-        if not os.path.isdir(folderpath):
-            continue
-
-        # Loop through each file in the folder
-        for filename in os.listdir(folderpath):
-            filepath = os.path.join(folderpath, filename)
-
-            # Skip any non-image files
-            if not filename.lower().endswith((".png", ".jpg", ".jpeg", ".bmp", ".gif")):
-                continue
-
-            # Move the image to the destination folder
-            shutil.move(filepath, destination_folder)
-            counter += 1
-    print("Moved {} images to {}.".format(counter, destination_folder))
-    print("Done!")
-
-# Credits to Project Sidewalk for the following function
+'''Credits to Project Sidewealk for the following function:
+https://github.com/ProjectSidewalk/sidewalk-panorama-tools/blob/master/DownloadRunner.py#L166'''
 def fetch_pano_ids_from_webserver():
     unique_ids = set()
     pano_info = []
@@ -85,26 +54,10 @@ def fetch_pano_ids_from_webserver():
 
 def main(args):
 
-    # Set the source and destination folder paths
-    source_folder = "download_data"
-    
-    # Make a directory specific for the neighbourhood
-    # Save the panoramas in the directory
-
-    # Replace everything that is not a character with an underscore in neighbourhood string, and make it lowercase
-    args.neighbourhood = re.sub(r'[^a-zA-Z]', '_', args.neighbourhood).lower()
-
-    destination_folder = os.path.join(args.output_dir, args.neighbourhood)
-    destination_folder = destination_folder + '_' + args.quality
-    if not os.path.exists(destination_folder):
-        os.makedirs(destination_folder, exist_ok=True)
-
-    move_panos(source_folder, destination_folder)
-
-    # Loop through destination_folder and collect the IDs of the panos
+    # Loop through args.input_dir and collect the IDs of the panos
     pano_ids = []
     # Loop through each folder in the source folder
-    for filename in os.listdir(destination_folder):
+    for filename in os.listdir(args.input_dir):
 
         # Skip any non-image files
         if not filename.lower().endswith((".png", ".jpg", ".jpeg", ".bmp", ".gif")):
@@ -116,15 +69,23 @@ def main(args):
 
     # Print the first ID
     print(f'First ID: {pano_ids[:1]}')
+    print(f'# of IDs: {len(pano_ids)}')
 
     # Connect to the API endpoint, collect information for each pano, get the heading and save the couple ID-heading in a .csv file
     pano_info = fetch_pano_ids_from_webserver()
 
+    # Print length of pano_info
+    print(f'# of IDs in pano_info before filtering: {len(pano_info)}')
+
     # Filter pano_info to only include the IDs we have in our dataset
     pano_info = [pano for pano in pano_info if pano['gsv_panorama_id'] in pano_ids]
 
+    # Print length of pano_info
+    print(f'# of IDs in pano_info after filtering: {len(pano_info)}')
+    assert(len(pano_info) == len(pano_ids))
+
     # Make a .csv file with the IDs and their headings
-    with open(f'{destination_folder}/panos.csv', mode='w') as csv_file:
+    with open(f'{args.input_dir}/panos.csv', mode='w') as csv_file:
         # Write the headers to the CSV file
         csv_file.write('pano_id,heading\n')
 
@@ -135,8 +96,10 @@ def main(args):
             except:
                 print(f"Error writing {pano['gsv_panorama_id']} to the CSV file.")
 
+        print(f'Done! panos.csv saved in {args.input_dir}.')
+
     # Make another .csv file with the IDs and their latitudes and longitudes
-    with open(f'{destination_folder}/panos_coords.csv', mode='w') as csv_file:
+    with open(f'{args.input_dir}/panos_coords.csv', mode='w') as csv_file:
         # Write the headers to the CSV file
         csv_file.write('pano_id,lat,lng\n')
 
@@ -147,13 +110,13 @@ def main(args):
             except:
                 print(f"Error writing {pano['gsv_panorama_id']} to the CSV file.")
 
+        print(f'Done! panos_coords.csv saved in {args.input_dir}.')
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
 
     parser.add_argument('--input_dir', type=str, default='res/dataset', help='input directory')
-    parser.add_argument('--neighbourhood', type=str, default='Osdorp', help='neighbourhood')
-    parser.add_argument('--quality', type=str, default='small', help='quality of the images')
 
     args = parser.parse_args()
 

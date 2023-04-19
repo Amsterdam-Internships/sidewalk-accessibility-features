2bdd7021de5e8bacd7148113eca2f71d5db3aaa4
imandrealombardo <60581994+imandrealombardo@users.noreply.github.com>
2023-04-19 11:13:57 +0200

    segment out classes from panos

--- segment_gd.py
+++ segment_gd.py
@@ -4,6 +4,9 @@ import numpy as np
 import argparse
 import re
 import os
+import concurrent.futures
+import psutil
+from tqdm import tqdm
 
 def blacken_labels(input_image_path, masks_path, json_data, labels_to_blacken, output_image_path):
     # Load the panoramic image and create a copy
@@ -30,14 +33,75 @@ def blacken_labels(input_image_path, masks_path, json_data, labels_to_blacken, o
 def main(args):
     # Replace everything that is not a character with an underscore in neighbourhood string, and make it lowercase
     args.neighbourhood = re.sub(r'[^a-zA-Z]', '_', args.neighbourhood).lower()
+
+    args.labels = args.labels.split('.')
+    labels_to_blacken = args.labels
+
+    folder_name = 'ground_seg_outputs'
+
+    if args.ps:
+        args.input_dir = os.path.join(args.input_dir, folder_name)
+        
+    else:
+        args.input_dir = os.path.join(args.input_dir, args.neighbourhood)
+        args.input_dir = args.input_dir + '_' + args.quality
+        args.input_dir = os.path.join(args.input_dir, folder_name)
+
+    def process_subfolder(subfolder, output_folder, labels_to_blacken):
+        # Save subfolder name
+        subfolder_name = os.path.basename(subfolder)
+
+        # Check if there are separate files for each direction
+        has_directions = any(any(direction in f for direction in \
+                             ["_front", "_back", "_right", "_left"]) for f in os.listdir(subfolder))
+
+        # Process each subfolder depending on the case
+        if has_directions:
+            # Case 2: Process files with directions
+            directions = ["front", "back", "right", "left"]
+            # Save all the files in a folder with subfolder_name as name
+            output_folder = os.path.join(output_folder, subfolder_name)
+            for direction in directions:
+                input_image_path = os.path.join(subfolder, f"raw_image_{direction}.jpg")
+                masks_path = os.path.join(subfolder, f"mask_{direction}.jpg")
+                json_path = os.path.join(subfolder, f"mask_{direction}.json")
+                output_image_path = os.path.join(output_folder, f"{direction}.png")
+
+                with open(json_path, "r") as json_file:
+                    json_data = json.load(json_file)
+
+                blacken_labels(input_image_path, masks_path, json_data, labels_to_blacken, output_image_path)
+        else:
+            # Case 1: Process files without directions
+            input_image_path = os.path.join(subfolder, f"raw_image_{subfolder_name}.jpg")
+            masks_path = os.path.join(subfolder, f"mask_{subfolder_name}.jpg")
+            json_path = os.path.join(subfolder, f"mask_{subfolder_name}.json")
+            output_image_path = os.path.join(output_folder, f"{subfolder_name}.jpg")
+
+            with open(json_path, "r") as json_file:
+                json_data = json.load(json_file)
+
+            blacken_labels(input_image_path, masks_path, json_data, labels_to_blacken, output_image_path)
+
+    # Define the input and output folders
+    input_folder = args.input_dir
     
-    # Go through each folder in the input directory
-    for folder in os.listdir(args.input_dir):
-        base_path = os.path.join(args.input_dir, folder)
-        blacken_labels(input_image_path, masks_path, json_data, labels_to_blacken, output_image_path)
-    
-    
-    return
+    root_folder = os.path.dirname(input_folder)
+    output_folder = os.path.join(root_folder, 'reoriented_seg')
+
+    # List all subfolders in the input folder
+    subfolders = [os.path.join(input_folder, d) for d in os.listdir(input_folder) if os.path.isdir(os.path.join(input_folder, d))]
+
+    # Calculate max_threads based on CPU capacity
+    cpu_count = psutil.cpu_count()
+    cpu_percent = psutil.cpu_percent()
+    max_threads = int((cpu_count * (1 - cpu_percent / 100)) * 0.5)
+    print(f'Using {max_threads} threads')
+
+    # Use ThreadPoolExecutor to limit the number of concurrent threads
+    with concurrent.futures.ThreadPoolExecutor(max_threads) as executor:
+        list(tqdm(executor.map(lambda subfolder: process_subfolder(subfolder, output_folder, labels_to_blacken), subfolders), total=len(subfolders)))
+
 
 if __name__ == '__main__':
 
@@ -45,21 +109,9 @@ if __name__ == '__main__':
     parser.add_argument('--input_dir', type=str, default='res/dataset')
     parser.add_argument('--neighbourhood', type=str, default='osdorp')
     parser.add_argument('--quality', type=str, default='full')
-    parser.add_argument('--visualize', type=bool, default=False, action=argparse.BooleanOptionalAction)
-    parser.add_argument('--threshold', type=int, default=100, help='Threshold for distance-based metrics (in pixels)')
-    parser.add_argument('--radius', type=int, default=100, help='Radius for point-to-mask best IoU (in pixels)')
     parser.add_argument('--ps', type=bool, default=True, action=argparse.BooleanOptionalAction)
-    parser.add_argument('--seg', type=bool, default=True, action=argparse.BooleanOptionalAction)
+    parser.add_argument('--labels', type=str, default='sky.building.car')
 
     args = parser.parse_args()
 
     main(args)
-
-    json_str = '...'
-    json_data = json.loads(json_str)
-    labels_to_blacken = ['sky', 'car']
-    input_image_path = 'path/to/panoramic_image.jpg'
-    masks_path = 'path/to/masks_image.jpg'
-    output_image_path = 'path/to/output_image.jpg'
-
-    blacken_labels(input_image_path, masks_path, json_data, labels_to_blacken, output_image_path)

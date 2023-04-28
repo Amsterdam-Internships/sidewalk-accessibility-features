57cd3101f2c51190e92c5f48b74529b223504de3
imandrealombardo <60581994+imandrealombardo@users.noreply.github.com>
2023-04-28 16:13:24 +0200

    generalize output path

--- prepare_csv.py
+++ prepare_csv.py
@@ -7,6 +7,8 @@ import requests
 import geopandas as gpd
 import json
 import pandas as pd
+import os
+import argparse
 
 def get_ps_labels():
 
@@ -36,32 +38,45 @@ def get_xy_coords_ps_labels():
 
     return other_labels_df
 
-# Get Project Sidewalk labels from API
-ps_labels_df = get_ps_labels()
-# Get the labels from another API call
-other_labels_df = get_xy_coords_ps_labels()
+def main(args):
+    # Get Project Sidewalk labels from API
+    ps_labels_df = get_ps_labels()
+    # Get the labels from another API call
+    other_labels_df = get_xy_coords_ps_labels()
 
-# Filter other_labels_df to only contain obstacles ('label_type_id' == 3)
-other_labels_df = other_labels_df[other_labels_df['label_type_id'] == 3]
+    # Filter other_labels_df to only contain obstacles ('label_type_id' == 3)
+    other_labels_df = other_labels_df[other_labels_df['label_type_id'] == 3]
 
-print(f'Number of obstacles labels from cvMetadata API (low quality): {len(other_labels_df)}')
+    print(f'Number of obstacles labels from cvMetadata API (low quality): {len(other_labels_df)}')
 
-# Intersect other_labels_df with ps_labels_df
-other_labels_df = other_labels_df[other_labels_df['label_id'].isin(ps_labels_df['label_id'])]
-print(f'Number of obstacles labels in other_labels_df after filtering for high quality data: {len(other_labels_df)}')
+    # Intersect other_labels_df with ps_labels_df
+    other_labels_df = other_labels_df[other_labels_df['label_id'].isin(ps_labels_df['label_id'])]
+    print(f'Number of obstacles labels in other_labels_df after filtering for high quality data: {len(other_labels_df)}')
 
-# Count the number of panos (unique gsv_panorama_id)
-print(f'Number of panos with obstacles labels: {len(other_labels_df["gsv_panorama_id"].unique())}')
+    # Count the number of panos (unique gsv_panorama_id)
+    print(f'Number of panos with obstacles labels: {len(other_labels_df["gsv_panorama_id"].unique())}')
 
-# 2. labels.csv
-# Save the filtered labels to a .csv file called labels.csv
-other_labels_df.to_csv('labels.csv', index=False)
+    # 2. labels.csv
+    # Save the filtered labels to a .csv file called labels.csv
+    labels_path = os.path.join(args.output_dir, 'labels.csv')
+    other_labels_df.to_csv(labels_path, index=False)
 
-# 1. panos_hq.csv
-# Save the filtered panos to a .csv file called panos_hq.csv using .unique
-# Replace "'" with "" in the gsv_panorama_id column
-other_labels_df['gsv_panorama_id'] = other_labels_df['gsv_panorama_id'].str.replace("'", "")
-# Save the filtered panos to a .csv file called panos_hq.csv
-# With each line containing a unique gsv_panorama_id
-other_labels_df['gsv_panorama_id'].unique().tofile('panos_hq.csv', sep='\n', format='%s')
+    # 1. panos_hq.csv
+    # Save the filtered panos to a .csv file called panos_hq.csv using .unique
+    # Replace "'" with "" in the gsv_panorama_id column
+    other_labels_df['gsv_panorama_id'] = other_labels_df['gsv_panorama_id'].str.replace("'", "")
+    # Save the filtered panos to a .csv file called panos_hq.csv
+    # With each line containing a unique gsv_panorama_id
+    panos_path = os.path.join(args.output_dir, 'panos_hq.csv')
+    other_labels_df['gsv_panorama_id'].unique().tofile(panos_path, sep='\n', format='%s')
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+   
+    parser.add_argument('--output_dir', type=str, default = 'download_PS')
+    
+    args = parser.parse_args()
+    
+    main(args)
 
